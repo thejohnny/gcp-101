8479be88682329f05a94835e229948ec1788c9f6
Nathan Dataguake Basanese <5750764+v6@users.noreply.github.com>
2019-04-17 17:23:07 -0700

    Add a comment to variables.tf
    Mention how to assign Terraform Variables using Operating System Environment Variables

--- variables.tf
+++ variables.tf
@@ -9,3 +9,9 @@ variable "billing_account" {  }
 variable "org_id" {  }
 
 variable "region" { default = 'us-west2' }
+
+
+##  Note: These can also be defined as follows: 
+##  export TF_VAR_my_variable_name="top-secret-value"
+##  For example, TF_VAR_account='{"type": "service_account",  "project_id": "unique-highway-237219"...
+##  Would correspond to the account variable
