5f82279fe6f8eb48aa8192085326b51aa77d10d9
Nathan Dataguake Basanese <5750764+v6@users.noreply.github.com>
2019-04-17 17:15:08 -0700

    Update variables.tf
    Add some sane defaults, where it makes sense to have a default value.

--- variables.tf
+++ variables.tf
@@ -1,11 +1,11 @@
-variable "console_num_instances" {}
+variable "console_num_instances" { default = 1 }
 
-variable "project_name" {}
+variable "project_name" { default = 'myrandomprojectname' }
 
-variable "project_id" {}
+variable "project_id" {  }  
 
-variable "billing_account" {}
+variable "billing_account" {  }
 
-variable "org_id" {}
+variable "org_id" {  }
 
-variable "region" {}
+variable "region" { default = 'us-west2' }
